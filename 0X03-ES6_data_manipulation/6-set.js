9b5a662b7d7e8289b83628a138b1d95effd474d7
webbtumelo <webbtumelo@gmail.com>
2024-04-29 08:49:20 -0700

    Data-manipulation

---
+++ 0X03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Tumelo Webb <https://github.com/webbtumelo>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
---
+++ 0X03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Retrieves a map of grocery names and their quantity.
+ * @author Tumelo Webb <https://github.com/webbtumelo>
+ * @returns {Map<string, Number>}
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
